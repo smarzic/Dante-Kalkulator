7473577c34ae24e6d624bf1a0454af7feaee0fcf
smarzic <sanjin.marzic@gmail.com>
2022-12-05 22:11:14 +0100

    Implementirane osnovne matematičke operacije

--- app/src/main/java/hr/dante_ri/kulator/MainActivity.java
+++ app/src/main/java/hr/dante_ri/kulator/MainActivity.java
@@ -2,12 +2,16 @@ package hr.dante_ri.kulator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int br1;
+    String operator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +35,48 @@ public class MainActivity extends AppCompatActivity {
             Button b = findViewById(buttons[i]);
             b.setOnClickListener(onClickListener);
         }
+
+        View.OnClickListener opOnClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // pročitati što je kliknuto
+                Button button = (Button) view;
+                operator = button.getText().toString();
+                // pročitati upisani broj, spremiti ga i isprazniti polje
+                TextView textView = findViewById(R.id.txtResult);
+                String currNumber = textView.getText().toString();
+                br1 = Integer.parseInt(currNumber);
+                textView.setText("");
+            }
+        };
+        int buttonsOp[] = {R.id.btnDivide, R.id.btnMinus, R.id.btnMultiply, R.id.btnPlus};
+        for(int i=0; i < buttonsOp.length; i++) {
+            Button b = findViewById(buttonsOp[i]);
+            b.setOnClickListener(opOnClickListener);
+        }
+
+        Button btnEquals = findViewById(R.id.btnEqual);
+        btnEquals.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // pročitati drugi broj
+                TextView textView = findViewById(R.id.txtResult);
+                String currText = textView.getText().toString();
+                int br2 = Integer.parseInt(currText);
+                // izračunati
+                int result = 0;
+                if(operator.equals("+")) {
+                    result = br1 + br2;
+                } else if(operator.equals("-")) {
+                    result = br1 - br2;
+                } else if(operator.equals("*")) {
+                    result = br1 * br2;
+                } else if(operator.equals("/")) {
+                    result = br1 / br2;
+                }
+                textView.setText(result + "");
+            }
+        });
 
     }
 }
